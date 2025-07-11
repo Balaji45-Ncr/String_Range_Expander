4a4fd4990e7b7a72f73dfa401a302583f320a344
Balaji45-Ncr <ncrlpb45@gmail.com>
2025-07-11 23:11:28 +0530

    stage1

--- test_range_expander.py
+++ test_range_expander.py
@@ -1,7 +1,14 @@
+import unittest
 from range_expander import RangeExpander
 
+class Testrangeexpander(unittest.TestCase):
+    def test_stage1_basic(self):
+        expander=RangeExpander()
+        result=expander.expand("1-3,5")
+        assert result == [1,2,3,5]
 
-def test_stage1_basic():
-    expander=RangeExpander()
-    result=expander.expand('1-3,5')
-    assert result == [1,2,3,5]
+
+if __name__=='__main__':
+    unittest.main()
+    print('test_stage2_basic  test cases passed')
+
